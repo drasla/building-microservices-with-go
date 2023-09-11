bc161ddc2f897b972bcb103c4b299c918553098a
Tae.Garam (Home) <drasla@naver.com>
2023-09-11 23:03:59 +0900

    cors

--- server.go
+++ server.go
@@ -1,83 +1,39 @@
 package main
 
 import (
-	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 )
-
-var name string
 
 type helloWorldResponse struct {
 	Message string `json:"message"`
 }
 
-type GzipHandler struct {
-	next http.Handler
-}
-
-type GzipResponseWriter struct {
-	gw *gzip.Writer
-	http.ResponseWriter
-}
-
 func main() {
 	port := 8080
-	http.Handle("/helloworld", NewGzipHandler(http.HandlerFunc(helloWorldHandler)))
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+	log.Fatal("Something")
 }
 
-func helloWorldHandler(rw http.ResponseWriter, r *http.Request) {
-	response := helloWorldResponse{Message: "Hello" + name}
-	encoder := json.NewEncoder(rw)
-	encoder.Encode(response)
-}
-
-func (h *GzipHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	encodings := r.Header.Get("Accept-Encoding")
-
-	if strings.Contains(encodings, "gzip") {
-		h.serveGzipped(w, r)
-	} else if strings.Contains(encodings, "deflate") {
-		panic("Deflate not implemented")
-	} else {
-		h.servePlain(w, r)
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "GET")
+		w.WriteHeader(http.StatusNoContent)
+		return
 	}
 
-}
-
-func (h *GzipHandler) serveGzipped(w http.ResponseWriter, r *http.Request) {
-	gzw := gzip.NewWriter(w)
-	defer gzw.Close()
-
-	w.Header().Set("Content-Encoding", "gzip")
-	h.next.ServeHTTP(GzipResponseWriter{gzw, w}, r)
-}
-
-func (h *GzipHandler) servePlain(w http.ResponseWriter, r *http.Request) {
-	h.next.ServeHTTP(w, r)
-}
-
-func NewGzipHandler(next http.Handler) http.Handler {
-	return &GzipHandler{next}
-}
-
-func (w GzipResponseWriter) Write(b []byte) (int, error) {
-	// 콘텐츠 타입이 설정되지 않은 경우, 압축되지 않은 본문에서 콘텐츠 타입을 유추한다.
-	if _, ok := w.Header()["Content-Type"]; !ok {
-		w.Header().Set("Content-Type", http.DetectContentType(b))
+	response := helloWorldResponse{Message: "Hello World"}
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("Oops")
 	}
-	return w.gw.Write(b)
-}
 
-func (w GzipResponseWriter) Flush() {
-	w.gw.Flush()
-	if fw, ok := w.ResponseWriter.(http.Flusher); ok {
-		fw.Flush()
-	}
+	fmt.Fprint(w, string(data))
 }
